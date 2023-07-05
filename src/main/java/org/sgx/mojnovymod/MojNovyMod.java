8086fc14e9e2d8b2ee0c289002692539202267b7
SonicallGameXGit <Sasha.Ambartsumov@Gmail.com>
2023-07-05 21:59:46 +0200

    Initial Commit

---
+++ src/main/java/org/sgx/mojnovymod/MojNovyMod.java
@@ -0,0 +1,8 @@
+package org.sgx.mojnovymod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MojNovyMod implements ModInitializer {
+    @Override
+    public void onInitialize() { }
+}
---
+++ src/main/java/org/sgx/mojnovymod/client/MojNovyModClient.java
@@ -0,0 +1,8 @@
+package org.sgx.mojnovymod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MojNovyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() { }
+}
